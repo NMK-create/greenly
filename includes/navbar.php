2d8e309a11e80a0cc57ef8092a9321f89415b4d6
nadja <nadja.merrild.k@hotmail.com>
2025-12-03 12:49:36 +0100

    indsat logo og stylet lidt på navbar. Der er ikke styr på teksten.

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,1 +1,18 @@
-<?php
+<nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">
+                <img src="img/websitelogo.png" alt="Logo" width="auto" height="80px" class="d-inline-block align-text-top">
+            </a>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="service.php">Service</a>
+                <a class="nav-link active" aria-current="page" href="omos.php">Om os</a>
+                <a class="nav-link active" aria-current="page" href="kontakt.php">Kontakt</a>
+                <button class="btn btn-BNCsec" type="submit">Få tilbud</button>
+            </div>
+        </div>
+    </div>
+</nav>
+
