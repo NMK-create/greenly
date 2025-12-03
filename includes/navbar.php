3605f568ce097f069277fee063225c8171825f92
nadja <nadja.merrild.k@hotmail.com>
2025-12-03 13:11:20 +0100

    der er indsat collapse på navbar. Den skal styles, så der er ordentlig kontrast. Der er stadig ikke styr på tekst.

--- includes/navbar.php
+++ includes/navbar.php
@@ -4,8 +4,11 @@
             <a class="navbar-brand" href="index.php">
                 <img src="img/websitelogo.png" alt="Logo" width="auto" height="80px" class="d-inline-block align-text-top">
             </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
         </div>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="collapse navbar-collapse" id="navbarScroll">
             <div class="navbar-nav">
                 <a class="nav-link active" aria-current="page" href="service.php">Service</a>
                 <a class="nav-link active" aria-current="page" href="omos.php">Om os</a>
