497b47899ff44b0c2cf44ca53a83f8d5f7ef5f07
nadja <nadja.merrild.k@hotmail.com>
2025-12-04 11:31:21 +0100

    indsat footer + baggrunds

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,10 @@
-<?php
+<footer class="footer mt-5 p-3">
+    <nav aria-label="Bundmenu">
+        <ul class="nav justify-content-start mb-3">
+            <li class="nav-item"><a class="nav-link" href="eaa.php">EAA</a></li>
+            <li class="nav-item"><a class="nav-link" href="esg.php">ESG</a></li>
+            <li class="nav-item"><a class="nav-link" href="dei.php">DEI</a></li>
+        </ul>
+    </nav>
+    <div class="container text-center"><p class="text-center mb-0">&copy; 2025 Greenly.</p></div>
+</footer>
