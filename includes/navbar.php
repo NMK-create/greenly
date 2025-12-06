e06d9fdbe99fa42655c833d130b63972f1a969ab
nadja <nadja.merrild.k@hotmail.com>
2025-12-06 21:15:56 +0100

    ryddet lidt op i navbar og css

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,48 +1,43 @@
-<nav class="navbar navbar-expand-lg d-flex justify-content-center py-3 mb-4" role="navigation" aria-label="Hovednavigation">
+<nav class="navbar navbar-expand-lg d-flex justify-content-center py-3 mb-4" role="navigation"
+     aria-label="Hovednavigation">
     <div class="container-fluid">
-        <div class="container">
-            <a class="navbar-brand" href="index.php" aria-label="Til forsiden">
-                <img src="img/websitelogo.png" alt="Logo som fører til forside" class="d-inline-block align-text-top">
-            </a>
 
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarScroll" aria-controls="navbarScroll"
-                    aria-expanded="false" aria-label="Åbn navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-        </div>
+
+        <a class="navbar-brand" href="index.php" aria-label="Til forsiden">
+            <img src="img/websitelogo.png" alt="Logo som fører til forside" class="d-inline-block align-text-top">
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarScroll" aria-controls="navbarScroll"
+                aria-expanded="false" aria-label="Åbn navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
 
         <div class="collapse navbar-collapse" id="navbarScroll">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link"
-                       href="service.php">Service</a>
+                    <a class="nav-link px-3" href="service.php">Service</a>
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link"
-                       href="om-os.php">Om os</a>
+                    <a class="nav-link px-3" href="om-os.php">Om os</a>
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link"
-                       href="kontakt.php">Kontakt</a>
+                    <a class="nav-link px-3" href="kontakt.php">Kontakt</a>
                 </li>
-
             </ul>
 
-            <a class="btn btn-BNCsec" href="kontakt.php">Få tilbud</a>
-        </div>
+            <a class="btn btn-BNCsec px-3 ms-3" href="kontakt.php">Få tilbud</a>
 
+            <button id="theme-toggle" class="btn btn-BNCsec px-3 ms-3" aria-label="Skift til mørk tilstand"
+                    data-bs-toggle="tooltip" data-bs-placement="left" title="Skift tema">
+                <i id="theme-icon" class="fa-sharp fa-solid fa-moon"></i>
+            </button>
+        </div>
     </div>
 </nav>
-
-<div class="container d-flex justify-content-end mt-2">
-    <button id="theme-toggle" class="btn btn-BNCsec" aria-label="Skift til mørk tilstand" data-bs-toggle="tooltip" data-bs-placement="left" title="Skift tema">
-        <i id="theme-icon" class="fa-sharp fa-solid fa-moon"></i>
-    </button>
-</div>
 
 
 <script>
