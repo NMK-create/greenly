1ef1f7b1a3cd5d05fbba4a765f48641335dabe94
minie08 <bea003@edu.zealand.dk>
2025-12-10 23:12:19 +0100

    footer ok

--- includes/footer.php
+++ includes/footer.php
@@ -1,33 +1,35 @@
-<footer class="footer mt-5 text-light">
-    <div class="footer-top container-fluid py-4">
+<footer class="footer mt-5" aria-label="Sidefod">
+    <div class="footer-top container-fluid pt-4">
         <div class="container">
             <div class="row justify-content-between">
 
                 <!-- Venstre-->
-                <div class="col-lg-6 col-md-6 mb-4">
+                <div class="col-lg-6 col-md-6">
                     <h4 class="footer-title">Greenly – Digitalt Bureau</h4>
-                    <p class="mb-1">Kommunikationsvej 123</p>
-                    <p class="mb-3">4200 Slagelse</p>
+                    <address class="mb-3">Kommunikationsvej 123<br>4200 Slagelse</address>
 
-                    <ul class="nav footer-menu">
-                        <li class="nav-item"><a class="nav-link" href="eaa.php">EAA</a></li>
-                        <li class="nav-item"><a class="nav-link" href="esg.php">ESG</a></li>
-                        <li class="nav-item"><a class="nav-link" href="dei.php">DEI</a></li>
+                    <ul class="nav footer-menu" aria-label="Footer menu">
+                        <li class="nav-item"><a class="nav-link" href="/eaa.php">EAA</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/esg.php">ESG</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/dei.php">DEI</a></li>
                     </ul>
                 </div>
 
                 <!-- Højre -->
-                <div class="col-lg-5 col-md-6 text-md-end">
+                <div class="col-lg-5 col-md-6 text-md-end my-3">
                     <p class="mb-1">
-                        <i class="fa-solid fa-phone"></i> Tlf: +45 52 52 44 35
+                        <i class="fa-solid fa-phone" aria-hidden="true"></i>
+                        <a href="tel:+45 52 52 44 35" class="footer-bottom-link">Tlf: +45 52 52 44 35</a>
                     </p>
                     <p class="mb-3">
-                        <i class="fa-solid fa-envelope"></i> Mail: info@greenly.dk
+                        <i class="fa-solid fa-envelope" aria-hidden="true"></i>
+                        <a href="mailto:info@greenly.dk" class="footer-bottom-link">Mail: info@greenly.dk</a>
                     </p>
 
                     <div class="social-icons">
                         <a href="#" aria-label="Facebook"><i class="fa-brands fa-facebook"></i></a>
                         <a href="#" aria-label="Instagram"><i class="fa-brands fa-instagram"></i></a>
+                        <a href="#" aria-label="Linkedin"><i class="fa-brands fa-linkedin"></i></a>
                     </div>
                 </div>
 
@@ -38,7 +40,7 @@
     <!-- Bundlinje -->
     <div class="footer-bottom py-2">
         <div class="container d-flex flex-column flex-md-row justify-content-between align-items-center">
-            <p class="mb-0">CVR: 123</p>
+            <p class="mb-0">CVR: 44662175</p>
             <div>
                 <a href="#" class="footer-bottom-link">Cookie & privatlivspolitik</a>
                 <a href="#" class="footer-bottom-link ms-3">Handelsbetingelser</a>
