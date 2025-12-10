d3869e33cb6c75246c6a8b39cbf201a2d195588e
minie08 <bea003@edu.zealand.dk>
2025-12-10 12:14:39 +0100

    ny billede

--- includes/footer.php
+++ includes/footer.php
@@ -1,14 +1,48 @@
-<footer class="footer container-fluid mt-5 p-3">
-    <section>
+<footer class="footer mt-5 text-light">
+    <div class="footer-top container-fluid py-4">
+        <div class="container">
+            <div class="row justify-content-between">
 
-    </section>
-    <section>
-        <ul class="nav justify-content-start mb-3">
-            <li class="nav-item"><a class="nav-link" href="eaa.php">EAA</a></li>
-            <li class="nav-item"><a class="nav-link" href="esg.php">ESG</a></li>
-            <li class="nav-item"><a class="nav-link" href="dei.php">DEI</a></li>
-        </ul>
-    </section>
+                <!-- Venstre kolonne -->
+                <div class="col-lg-6 col-md-6 mb-4">
+                    <h4 class="footer-title">Greenly – Digitalt Bureau</h4>
+                    <p class="mb-1">Kommunikationsvej 123</p>
+                    <p class="mb-3">4200 Slagelse</p>
 
-        <div class="container-fluid footer-card"><p class="justify-content-start">&copy; 2025 Greenly.</p></div>
+                    <ul class="nav footer-menu">
+                        <li class="nav-item"><a class="nav-link" href="eaa.php">EAA</a></li>
+                        <li class="nav-item"><a class="nav-link" href="esg.php">ESG</a></li>
+                        <li class="nav-item"><a class="nav-link" href="dei.php">DEI</a></li>
+                    </ul>
+                </div>
+
+                <!-- Højre kolonne -->
+                <div class="col-lg-5 col-md-6 text-md-end">
+                    <p class="mb-1">
+                        <i class="fa-solid fa-phone"></i> Tlf: +45 52 52 44 35
+                    </p>
+                    <p class="mb-3">
+                        <i class="fa-solid fa-envelope"></i> Mail: info@greenly.dk
+                    </p>
+
+                    <div class="social-icons">
+                        <a href="#" aria-label="Facebook"><i class="fa-brands fa-facebook"></i></a>
+                        <a href="#" aria-label="Instagram"><i class="fa-brands fa-instagram"></i></a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <!-- Bundlinje -->
+    <div class="footer-bottom py-2">
+        <div class="container d-flex flex-column flex-md-row justify-content-between align-items-center">
+            <p class="mb-0">CVR: 123</p>
+            <div>
+                <a href="#" class="footer-bottom-link">Cookie & privatlivspolitik</a>
+                <a href="#" class="footer-bottom-link ms-3">Handelsbetingelser</a>
+            </div>
+        </div>
+    </div>
 </footer>
